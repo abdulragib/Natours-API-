a8950e8fea995970f991e858d79dcb17c13c747d
Abdul Ragib <abdulragib36@gmail.com>
2023-12-24 22:10:09 +0530

    Patch request done

--- app.js
+++ app.js
@@ -32,11 +32,11 @@ app.get('/api/v1/tours/:id', (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
-  if(!tour){
+  if (!tour) {
     return res.status(404).json({
       status: 'fail',
-      message:"Invalid ID"
-    })
+      message: 'Invalid ID',
+    });
   }
 
   res.status(200).json({
@@ -65,6 +65,22 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if ((req.params.id*1) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Update Tour here>',
+    },
+  });
 });
 
 const port = 3000;
