231e6d26f0bd7cf350a73fc65a9caeb2a2a2a452
Abdul Ragib <abdulragib36@gmail.com>
2023-12-26 10:54:19 +0530

    bug fix

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -59,13 +59,13 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  // console.log(req.body);
+  console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
       res.status(201).json({
