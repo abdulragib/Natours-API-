a1167b5a54d7d45e756d5197b8eeaa7d8b9a45b9
Abdul Ragib <abdulragib36@gmail.com>
2023-12-30 06:28:14 +0530

    tourSchema made

--- server.js
+++ server.js
@@ -30,6 +30,9 @@ const tourSchema = new mongoose.Schema({
   },
 });
 
+const Tour=mongoose.model('Tour',tourSchema);
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App Running on Port ${port}...`);
