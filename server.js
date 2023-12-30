5fa5fa3ca2e54415780f9ccf47ee8262e6bc89ce
Abdul Ragib <abdulragib36@gmail.com>
2023-12-30 06:10:05 +0530

    MongoDB connection done

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then((con) => {
+  // console.log(con.connections);
+  console.log('DB Connection Sucessful!');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
