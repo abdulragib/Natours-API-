9ac15ed59c596e48121ce7e3f7c02fc59181e1d3
Abdul Ragib <abdulragib36@gmail.com>
2023-12-31 12:34:24 +0530

    schema updated

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,26 +1,62 @@
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, 'A tour must have name'],
-      unique: true,
-    },
-    rating: {
-      type: Number,
-      default: 4.5,
-    },
-    price: {
-      type: Number,
-      required: [true, 'A tour must have price'],
-    },
-  });
-  
-  const Tour = mongoose.model('Tour', tourSchema);
-  const testTour = new Tour({
-    name: 'The Mountain Hiker',
-    rating: 4.7,
-    price: 497,
-  });
+  name: {
+    type: String,
+    required: [true, 'A tour must have name'],
+    unique: true,
+  },
+  durations: {
+    type: Number,
+    required: [true, 'A tour must have duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date]
+});
 
-module.exports=Tour;
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'The Mountain Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+module.exports = Tour;
