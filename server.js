eda80a7431d5ba1ed45859a10d5fa8cb569a86a1
Abdul Ragib <abdulragib36@gmail.com>
2024-01-17 12:30:44 +0530

    uncaught exception handled

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 🔥 Shutting down..');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -20,10 +26,7 @@ app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLER REJECTION! 🔥 Shutting down..');
-  server.close(()=>{
-    process.exit(1);
-  })
-  
+  console.log(err.name, err.message);
+  process.exit(1);
 });
