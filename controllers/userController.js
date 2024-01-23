5c82a2b90375e9c0cd6a13b7b637124c4fe9d01e
Abdul Ragib <abdulragib36@gmail.com>
2024-01-23 21:28:13 +0530

    Sign in user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,17 @@
+const User = require('../models/userModal');
+const catchAsync = require('./../Utils/catchAsync');
 
-  
+exports.getAllUsers = catchAsync(async(req, res,next) => {
+  const users = await User.find();
 
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
