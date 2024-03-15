27d05bc599177c021bd1fbcf29512a9091ee6740
Abdul Ragib <abdulragib36@gmail.com>
2024-03-15 11:54:56 +0530

    Statics method

--- models/reviewModal.js
+++ models/reviewModal.js
@@ -1,4 +1,5 @@
 const { default: mongoose } = require('mongoose');
+const Tour=require('./tourModal');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -46,6 +47,30 @@ reviewSchema.pre(/^find/,function(next){
         select:'name photo'
     })
     next();
+})
+
+//statics for review created on tour
+reviewSchema.statics.calcAverageRatings= async function(tourId){
+  const stats= await this.aggregate([
+    {
+      $match:{tour:tourId}
+    },
+    {$group:{
+      _id:'$tour',
+      nRating:{$sum:1},
+      avgRating:{$avg:'$rating'}
+    }}
+  ]);
+  console.log(stats);
+  await Tour.findByIdAndUpdate(tourId,{
+    ratingsQuantity:stats[0].nRating,
+    ratingsAverage:stats[0].avgRating
+  })
+}
+
+reviewSchema.post('save',function(){
+  //this points to current review
+  this.constructor.calcAverageRatings(this.tour);
 })
 
 const Review = mongoose.model('Review', reviewSchema);
