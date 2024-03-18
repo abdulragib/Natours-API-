9457b96ab7fc34a94c91af6d3c04cbf1d56dbe70
Abdul Ragib <abdulragib36@gmail.com>
2024-03-18 13:00:48 +0530

    updating ratings average when updating the review

--- models/reviewModal.js
+++ models/reviewModal.js
@@ -1,5 +1,5 @@
 const { default: mongoose } = require('mongoose');
-const Tour=require('./tourModal');
+const Tour = require('./tourModal');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -33,45 +33,65 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
-reviewSchema.pre(/^find/,function(next){
-    // this.populate({
-    //     path:'tour',
-    //     select:'name'
-    // }).populate({
-    //     path:'user',
-    //     select:'name photo'
-    // })
+reviewSchema.pre(/^find/, function (next) {
+  // this.populate({
+  //     path:'tour',
+  //     select:'name'
+  // }).populate({
+  //     path:'user',
+  //     select:'name photo'
+  // })
 
-   this.populate({
-        path:'user',
-        select:'name photo'
-    })
-    next();
-})
+  this.populate({
+    path: 'user',
+    select: 'name photo',
+  });
+  next();
+});
 
 //statics for review created on tour
-reviewSchema.statics.calcAverageRatings= async function(tourId){
-  const stats= await this.aggregate([
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
     {
-      $match:{tour:tourId}
+      $match: { tour: tourId },
     },
-    {$group:{
-      _id:'$tour',
-      nRating:{$sum:1},
-      avgRating:{$avg:'$rating'}
-    }}
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
   ]);
-  console.log(stats);
-  await Tour.findByIdAndUpdate(tourId,{
-    ratingsQuantity:stats[0].nRating,
-    ratingsAverage:stats[0].avgRating
-  })
-}
 
-reviewSchema.post('save',function(){
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
   //this points to current review
   this.constructor.calcAverageRatings(this.tour);
-})
+});
+
+// reviewSchema.pre(/^findOneAnd/, async function (next) {
+//   this.r = await this.findOne();
+//   console.log(this.r);
+//   next();
+// });
+
+reviewSchema.post(/^findOneAnd/, async function (doc) {
+  //await this.findOne(); does NOT work here, query has already executed
+  await doc.constructor.calcAverageRatings(doc.tour);
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 
