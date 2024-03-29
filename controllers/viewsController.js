e31f5f819fe092d8831b4ba8ccba9d05e4a748a4
Abdul Ragib <abdulragib36@gmail.com>
2024-03-29 10:39:14 +0530

    building tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModal');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res,next) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   //1) Get tour data from collection
   const tours = await Tour.find();
   //2) Build template
@@ -13,8 +13,17 @@ exports.getOverview = catchAsync(async (req, res,next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  //1) get the data, for the request tour (including review and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  //2) Build template
+
+  //3) Render template using data from 1)
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour:tour
   });
-};
+});
